fc007cd1628e77cd3aabdc21cb2bfd8acd896dad
kkznch <kkznch710md@gmail.com>
2020-03-22 16:10:50 +0900

    third commit

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,3 +1,37 @@
+const fetch = require('node-fetch');
+
+const requestGithubToken = credentials => {
+  return fetch(
+    'https://github.com/login/oauth/access_token',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    }
+  )
+    .then(res => res.json())
+    .catch(error => {
+      throw new Error(JSON.stringify(error));
+    });
+};
+
+const requestGithubUserAccount = token => {
+  return fetch(`https://api.github.com/user?access_token=${token}`)
+    .then(res => res.json())
+    .catch(error => {
+      throw new Error(JSON.stringify(error));
+    });
+};
+
+async function authorizeWithGithub(credentials) {
+  const { access_token } = await requestGithubToken(credentials);
+  const githubUser = await requestGithubUserAccount(access_token);
+  return { ...githubUser, access_token };
+}
+
 module.exports = {
   postPhoto(parent, args) {
     let newPhoto = {
@@ -8,4 +42,32 @@ module.exports = {
     photos.push(newPhoto);
     return newPhoto;
   },
+  async githubAuth(parent, { code }, { db }) {
+    let {
+      message,
+      access_token,
+      avatar_url,
+      login,
+      name
+    } = await authorizeWithGithub({
+      client_id: process.env.GITHUB_CLIENT,
+      client_secret: process.env.GITHUB_SECRET,
+      code,
+    });
+
+    if (message) {
+      throw new Error(message);
+    }
+
+    let latestUserInfo = {
+      name,
+      githubLogin: login,
+      githubToken: access_token,
+      avatar: avatar_url,
+    };
+
+    const { ops: [user] } = await db.collection('users').replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true })
+
+    return { user, token: access_token };
+  }
 };
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,4 +1,6 @@
 module.exports = {
-  totalPhotos: () => photos.length,
-  allPhotos: (parent, args) => photos
+  totalPhotos: (parent, args, { db }) => db.collection('photos').estimatedDocumentCount(),
+  allPhotos: (parent, args, { db }) => db.collection('photos').find().toArray(),
+  totalUsers: (parent, args, { db }) => db.collection('users').estimatedDocumentCount(),
+  allUsers: (parent, args, { db }) => db.collection('users').find().toArray(),
 };
